4fca656d3fdd998dfbf41eb1d601006e830cc9c2
JanainaFS <janainafeitosa@aluno.fapce.edu.br>
2023-04-28 15:47:08 -0300

    :white_check_mark: Seção 3 - DUplicação de código - Exercício

--- cypress/e2e/duplication/sample1.cy.js
+++ cypress/e2e/duplication/sample1.cy.js
@@ -1,5 +1,6 @@
 describe('Code duplication bad practice - repetitive steps', () => {
-  it('searches by typing and hitting enter', () => {
+
+  beforeEach(() => {
     cy.intercept(
       'GET',
       '**/search**'
@@ -10,29 +11,23 @@ describe('Code duplication bad practice - repetitive steps', () => {
 
     cy.get('input[type="text"]')
       .should('be.visible')
+      .as('searchField')
       .and('have.value', 'redux')
       .clear()
-      .type('frontend testing{enter}')
+
+  })
+  it('searches by typing and hitting enter', () => {
+    cy.get('@searchField')  
+    .type('frontend testing{enter}')
 
     cy.wait('@getStories')
 
     cy.get('.table-row')
-      .should('have.length', 100)
+      .should('have.length', 100) 
   })
 
   it('searches by typing and pressing the search button', () => {
-    cy.intercept(
-      'GET',
-      '**/search**'
-    ).as('getStories')
-
-    cy.visit('https://hackernews-seven.vercel.app')
-    cy.wait('@getStories')
-
-    cy.get('input[type="text"]')
-      .should('be.visible')
-      .and('have.value', 'redux')
-      .clear()
+    cy.get('@searchField') 
       .type('frontend testing')
 
     cy.contains('button', 'Search')
